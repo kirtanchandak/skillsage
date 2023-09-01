ef4d44c98a23bc0d5eab99478487cc6dff9eee71
kirtanchandak <themarketingkirtan@gmail.com>
2023-09-01 20:13:37 +0530

    courses page design

--- skillsage-frontend/src/pages/Courses.jsx
+++ skillsage-frontend/src/pages/Courses.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import Layout from "../components/Layout";
 
 function Courses() {
   const [courses, setCourses] = useState([]);
@@ -7,7 +8,12 @@ function Courses() {
     const getCourses = async () => {
       try {
         const response = await axios.get(
-          "http://localhost:3000/admin/allcourses"
+          "http://localhost:3000/admin/courses",
+          {
+            headers: {
+              Authorization: `Bearer ${localStorage.getItem("token")}`,
+            },
+          }
         );
         setCourses(response.data.courses);
       } catch (err) {
@@ -18,20 +24,51 @@ function Courses() {
   }, []);
 
   return (
-    <div>
-      <h1>Courses</h1>
-      <div className="grid grid-cols-1 lg:grid-cols-2  gap-5">
-        {courses.map((course) => (
-          <div key={course._id} className="bg-gray-500">
-            <h1>{course.title}</h1>
-            <p>{course.description}</p>
-            <p>Price: {course.price}</p>
-            <button className="bg-red-200">Buy</button>
+    <>
+      <Layout>
+        <div className="mt-8">
+          <h1 className="text-center text-5xl font-dm-serif-display">
+            Courses
+          </h1>
+          <div className="flex justify-center items-center">
+            <div className="grid grid-cols-1 lg:grid-cols-2 gap-5 mt-8">
+              {courses.map((course) => (
+                <Course course={course} />
+              ))}
+            </div>
           </div>
-        ))}
+        </div>
+      </Layout>
+    </>
+  );
+}
+
+const Course = (props) => {
+  return (
+    <div key={props.course._id} className="bg-gray-500 font-poppins">
+      <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+        <a href="#">
+          <img class="" src={props.course.imageUrl} alt="" />
+        </a>
+        <div class="p-5">
+          <a href="#">
+            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+              {props.course.title}
+            </h5>
+          </a>
+          <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
+            {props.course.description}
+          </p>
+          <a
+            href="#"
+            class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white  rounded-lg bg-primary"
+          >
+            Edit Course
+          </a>
+        </div>
       </div>
     </div>
   );
-}
+};
 
 export default Courses;
